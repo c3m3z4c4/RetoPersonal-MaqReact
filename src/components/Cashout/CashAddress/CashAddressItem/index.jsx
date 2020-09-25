8a15fa289f5934252ded3eaa64565146fd1f0fe6
C3M3Z4 <cmeza@mezadigital.com>
2020-09-24 22:00:16 -0500

    Maquetado funcional de metodo de pago

--- src/components/Cashout/CashAddress/CashAddressItem/index.jsx
+++ src/components/Cashout/CashAddress/CashAddressItem/index.jsx
@@ -7,11 +7,13 @@ class CashAddressItem extends React.Component {
   return (
       <div className="container-item">
           <div className="item-Adrr">
-            <h4>{this.props.title}</h4>
-            <span>{this.props.address}</span>
-          </div>
-          <div className="item-Sellectform-check">
-              <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..."/>
+            <div className="item-addr-text">
+              <h4>{this.props.title}</h4>
+              <span>{this.props.address}</span>
+            </div>
+            <div className="item-Sellect form-check">
+                <input className="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="..."/>
+            </div>
           </div>
       </div>
     )
